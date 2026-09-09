38574ebecb5f7095bd1c8cb982c5e2a5c89c6f71
Liber <123596041+ATOM60@users.noreply.github.com>
2026-09-10 02:35:01 +0300

    Enable shared Explorer folder picker

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .image_gallery_core import *
+from . import native_folder_picker_backend  # noqa: F401
 from . import output_video_gallery_backend  # noqa: F401
 from . import output_video_folder_backend  # noqa: F401
 from . import output_video_cpu_backend  # noqa: F401
