f88379b3926cb871bdb4a7d5383724d14d693d9a
Liber <123596041+ATOM60@users.noreply.github.com>
2026-09-10 00:19:29 +0300

    Use Explorer-style picker for input gallery

--- __init__.py
+++ __init__.py
@@ -2,4 +2,3 @@ from .image_gallery_core import *
 from . import output_video_gallery_backend  # noqa: F401
 from . import output_video_folder_backend  # noqa: F401
 from . import output_video_cpu_backend  # noqa: F401
-from . import native_folder_picker_backend  # noqa: F401
